7cff3ba7b95ee928b1f28676b6c6c44d57b9829b
iamahacker1 <nguyenhoainam29.08.01@gmail.com>
2024-04-21 23:45:42 +0700

    update sql

--- shopapp-backend/database.sql
+++ shopapp-backend/database.sql
@@ -1,4 +1,110 @@
-create database shopapp;
-use shopapp;
+CREATE DATABASE shopapp;
+USE shopapp;
 
-create table users()
+CREATE TABLE users(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    fullname VARCHAR(100) DEFAULT '',
+    phonr_number VARCHAR(10) NOT NULL,
+    address VARCHAR(200) DEFAULT '',
+    password VARCHAR(100) NOT NULL DEFAULT '',
+    created_at DATETIME,
+    updated_at DATETIME,
+    is_active TINYINT(1) DEFAULT 1,
+    date_of_birth DATE,
+    facebook_account_id INT DEFAULT 0,
+    google_account_id INT DEFAULT 0
+);
+
+ALTER TABLE users
+ADD COLUMN role_id INT
+
+CREATE TABLE roles(
+    id INT PRIMARY KEY,
+    name VARCHAR(20) NOT NULL
+);
+
+ALTER TABLE users
+ADD FOREIGN KEY (role_id) REFERENCES roles(id)
+
+CREATE TABLE tokens(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    token VARCHAR(255) UNIQUE NOT NULL,
+    token_type VARCHAR(50) NOT NULL,
+    expiration_date DATETIME,
+    revoked TINYINT(1) NOT NULL,
+    expired TINYINT(1) NOT NULL,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+--login with facebook and google
+CREATE TABLE social_accounts(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    provider VARCHAR(20) NOT NULL COMMENT 'Tên nhà social network',
+    provider_id VARCHAR(50) NOT NULL,
+    email VARCHAR(150) NOT NULL COMMENT 'Email tài khoản',
+    name VARCHAR(100) NOT NULL COMMENT 'Tên người dùng',
+    user_id int,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+--category table--
+CREATE TABLE categories(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL DEFAULT '' COMMENT 'Tên danh mục, vd: đồ điện tử'
+);
+
+--product table--
+CREATE TABLE products(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(350) COMMENT 'Tên sản phẩm',
+    price FLOAT NOT NULL CHECK (price >= 0),
+    thumbnail VARCHAR(300) DEFAULT '',
+    description LONGTEXT DEFAULT '',
+    created_at DATETIME,
+    updated_at DATETIME,
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+--Order--
+CREATE TABLE orders(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    fullname VARCHAR(100) DEFAULT '',
+    email VARCHAR(100) DEFAULT '',
+    phonr_number VARCHAR(200) NOT NULL,
+    address VARCHAR(200) NOT NULL,
+    note VARCHAR(100) DEFAULT '',
+    order_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(20),
+    total_money FLOAT CHECK (total_money >= 0),
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+ALTER TABLE orders ADD COLUMN `shipping_method` VARCHAR(100);
+ALTER TABLE orders ADD COLUMN `shipping_address` VARCHAR(200);
+ALTER TABLE orders ADD COLUMN `shipping_date` DATE;
+ALTER TABLE orders ADD COLUMN `shipping_number` VARCHAR(100);
+ALTER TABLE orders ADD COLUMN `payment_method` VARCHAR(100);
+
+--Xóa 1 đơn hàng => xóa mềm
+ALTER TABLE orders ADD COLUMN active TINYINT(1);
+
+--Trạng thái đơn hàng chỉ được nhận một giá trị cụ thể
+ALTER TABLE orders
+MODIFY COLUMN status ENUM('pending', 'processing', 'shipped', 'delivered', 'cancelled')
+COMMENT 'trạng thái đơn hàng';
+
+--Order detail--
+CREATE TABLE order_detail(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT,
+    FOREIGN KEY (order_id) REFERENCES orders(id),
+    product_id INT,
+    FOREIGN KEY (product_id) REFERENCES products(id),
+    price FLOAT CHECK (price >=0),
+    number_of_products int CHECK (number_of_products > 0),
+    total_money FLOAT CHECK (total_money >= 0),
+    color VARCHAR(20) DEFAULT ''
+);
