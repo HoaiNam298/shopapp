0dae3c903ebe5c6cbba71f9300be035155c0e872
iamahacker1 <nguyenhoainam29.08.01@gmail.com>
2024-06-14 01:37:25 +0700

    2 way data binding form register

--- shopapp-angular/src/app/register/register.component.ts
+++ shopapp-angular/src/app/register/register.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
 import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,12 +12,14 @@ import { FormsModule } from '@angular/forms';
   imports: [RouterOutlet,
     FooterComponent,
     HeaderComponent,
-    FormsModule
+    FormsModule,
+    NgIf
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  @ViewChild('registerForm') registerForm!: NgForm;
   phone: string;
   password: string;
   retypePassword: string;
@@ -49,5 +53,29 @@ export class RegisterComponent {
                     `dateOfBirth: ${this.dateOfBirth}` +
                     `isAccepted: ${this.isAccepted}`;
     alert(message)
+  }
+  checkPasswordsMatch(){
+    if(this.password !== this.retypePassword) {
+      this.registerForm.form.controls['retypePassword'].setErrors({'paswordMismatch': true});
+    } else {
+      this.registerForm.form.controls['retypePassword'].setErrors(null);
+    }
+  }
+  checkAge() {
+    if(this.dateOfBirth) {
+      const today = new Date();
+      const birthDate = new Date(this.dateOfBirth);
+      let age = today.getFullYear() - birthDate.getFullYear();
+      const monthDiff = today.getMonth() - birthDate.getMonth();
+      if(monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+      }
+
+      if(age < 18) {
+        this.registerForm.form.controls['dateOfBirth'].setErrors({'invalidAge': true});
+      } else {
+        this.registerForm.form.controls['dateOfBirth'].setErrors(null);
+      }
+    }
   }
 }
