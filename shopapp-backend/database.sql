1a0dd5067ec7ae0cc43603011582147048bbf936
iamahacker1 <nguyenhoainam29.08.01@gmail.com>
2025-01-04 23:58:06 +0700

    Update database.sql

--- shopapp-backend/database.sql
+++ shopapp-backend/database.sql
@@ -126,7 +126,8 @@ CREATE TABLE comments(
     product_id INT,
     user_id INT,
     content VARCHAR(255),
-    create_at DATETIME,
+    created_at DATETIME,
+    updated_at DATETIME,
 	FOREIGN KEY (product_id) REFERENCES products(id),
 	FOREIGN KEY (user_id) REFERENCES users(id)
 );
